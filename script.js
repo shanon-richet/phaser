407f5088a9f7255aefe82c332c6a2fb1a6dcdb85
Shanon <shanon.richet@gmail.com>
2022-03-28 14:55:34 +0200

    initialisation du jeu

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 
-    null, {
+const main = document.querySelector('main')
+const grid = document.querySelector('.grid')
+const sequence = document.querySelector('.sequence')
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 
+    'phaser', {
         create: create, 
         preload: preload,
+        render: render,
         update: update
     })
+
     var background
     
     function preload() {
-    game.load.spritesheet('forme', 'assets/forme1.png')
-    game.load.spritesheet('forme', 'assets/forme2.png')
-    game.load.spritesheet('forme', 'assets/forme3.png')
-    game.load.spritesheet('forme', 'assets/forme4.png')
-    game.load.spritesheet('forme', 'assets/forme5.png')
-    game.load.spritesheet('forme', 'assets/forme6.png')
-    
-    game.stage.backgroundColor = "#fcba03"
+        game.stage.backgroundColor = "#fcba03"
+        for (var i = 0; i < 6; i++) {
+            game.load.image(`forme${i}`, `assets/forme${i}.png`)       
+        } 
     }
     function create() {
-        var group = game.add.group()
-        group.createMultiple(24, 'forme', [0, 1, 2, 3, 4], true)
-        group.align(12, -1, 20, 20)
-           /*forme1 = game.add.image(0, 200, `forme1`)
-           forme1 = game.add.image(100, 200, `forme2`)
-           forme1 = game.add.image(200, 200, `forme3`)
-           forme1 = game.add.image(300, 200, `forme4`)
-           forme1 = game.add.image(400, 200, `forme5`)
-           forme1 = game.add.image(500, 200, `forme6`)*/
-        }
-    
+        cases_1 = new Phaser.Rectangle(20, 50, 100, 100)
+        cases_2 = new Phaser.Rectangle(150, 50, 100, 100)
+        cases_3 = new Phaser.Rectangle(280, 50, 100, 100)
+
+        var formes = []
+        var sprite1 = game.add.sprite(70, 250, 'forme1')
+        var sprite2 = game.add.sprite(0, 0, 'forme2').alignTo(sprite1, Phaser.RIGHT_CENTER, 16)
+        var sprite3 = game.add.sprite(0, 0, 'forme3').alignTo(sprite2, Phaser.RIGHT_CENTER, 16)
+        var sprite4 = game.add.sprite(0, 0, 'forme4').alignTo(sprite3, Phaser.RIGHT_CENTER, 16)
+        var sprite5 = game.add.sprite(0, 0, 'forme5').alignTo(sprite4, Phaser.RIGHT_CENTER, 16)
+        var sprite6 = game.add.sprite(0, 0, 'forme6').alignTo(sprite5, Phaser.RIGHT_CENTER, 16)
+
+        grid_case_1 = new Phaser.Rectangle()
+        for (var i = 0; i < 6; i++) {
+            formes.push(`forme${i}`)
+        }   
+
+
+    }
+    function render() {
+        game.debug.geom(cases_1, '#03f8fc')
+        game.debug.geom(cases_2, '#03f8fc')
+        game.debug.geom(cases_3, '#03f8fc')
+
+    }    
+
     function update() {
-    
     }
 
-    for (let i = 0; i < 6; i++)
