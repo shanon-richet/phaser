9779d1102fa75d440be77655e865e94b8ae1f746
Shanon <shanon.richet@gmail.com>
2022-03-28 15:42:20 +0200

    add phaser grid

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-color : https://coolors.co/540d6e-427aa1-ffd23f-f3fcf0-ef5d60
+## color : 
+- https://coolors.co/540d6e-427aa1-ffd23f-f3fcf0-ef5d60
+
+## source : 
+- https://www.3dxo.com/textures/3001_wall_tiles
--- script.js
+++ script.js
@@ -10,33 +10,39 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS,
         update: update
     })
 
-    var background
-    
-    function preload() {
-        game.stage.backgroundColor = "#fcba03"
-        for (var i = 0; i < 6; i++) {
-            game.load.image(`forme${i}`, `assets/forme${i}.png`)       
-        } 
-    }
+var background
+
+function preload() {
+    game.stage.backgroundColor = "#fcba03"
+    for (var i = 0; i < 7; i++) {
+        game.load.image(`forme${i}`, `assets/forme${i}.png`)  
+        game.load.spritesheet('formes', `assets/forme${i}.png`, 32, 32)
+    }  
+}
+
     function create() {
+        var group = game.add.group();
+        group.createMultiple(4, ['forme1', 'forme2', 'forme3', 'forme4', 'forme5', 'forme6'], [0, 2], true);
+        group.align(10, 10, 100, 100)
+
+        group.x = 100;
+        group.y = 200;
         cases_1 = new Phaser.Rectangle(20, 50, 100, 100)
         cases_2 = new Phaser.Rectangle(150, 50, 100, 100)
         cases_3 = new Phaser.Rectangle(280, 50, 100, 100)
 
         var formes = []
-        var sprite1 = game.add.sprite(70, 250, 'forme1')
-        var sprite2 = game.add.sprite(0, 0, 'forme2').alignTo(sprite1, Phaser.RIGHT_CENTER, 16)
-        var sprite3 = game.add.sprite(0, 0, 'forme3').alignTo(sprite2, Phaser.RIGHT_CENTER, 16)
-        var sprite4 = game.add.sprite(0, 0, 'forme4').alignTo(sprite3, Phaser.RIGHT_CENTER, 16)
-        var sprite5 = game.add.sprite(0, 0, 'forme5').alignTo(sprite4, Phaser.RIGHT_CENTER, 16)
-        var sprite6 = game.add.sprite(0, 0, 'forme6').alignTo(sprite5, Phaser.RIGHT_CENTER, 16)
+        game.add.image(0, 0, 'forme1')
+        game.add.image(100, 0, 'forme2')
+        game.add.image(200, 0, 'forme3')
+        game.add.image(300, 0, 'forme4')
+        game.add.image(400, 0, 'forme5')
+        game.add.image(500, 0, 'forme6')
 
-        grid_case_1 = new Phaser.Rectangle()
         for (var i = 0; i < 6; i++) {
             formes.push(`forme${i}`)
         }   
-
-
+        
     }
     function render() {
         game.debug.geom(cases_1, '#03f8fc')
@@ -47,4 +53,3 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS,
 
     function update() {
     }
-
