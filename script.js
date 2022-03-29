a4ac733dc55aa42264e973e3b82e6b3c16f078dd
Shanon <shanon.richet@gmail.com>
2022-03-29 09:46:46 +0200

    add random points

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var game = new Phaser.Game(1000, 600, Phaser.CANVAS,
 var background
 const form_list = []
 function preload() {
-    game.stage.backgroundColor = "#fcba03"
+    game.stage.backgroundColor = "#427AA1"
     for (var i = 0; i < 7; i++) {
         game.load.image(`forme${i}`, `assets/forme${i}.png`)  
         const list = game.load.spritesheet('formes', `assets/forme${i}.png`, 32, 32)
@@ -22,15 +22,17 @@ function preload() {
 }
 function create() {
         var form_grid = game.add.group();
-        form_grid.createMultiple(2, 
-            [`forme${[Math.floor(Math.random() * 6)]}`, 
-            `forme${[Math.floor(Math.random() * 6)]}`, 
-            `forme${[Math.floor(Math.random() * 6)]}`, 
-            `forme${[Math.floor(Math.random() * 6)]}`, 
-            `forme${[Math.floor(Math.random() * 6)]}`, 
-            `forme${[Math.floor(Math.random() * 6)]}`], [0, 1], true);
-        form_grid.align(5, 5, 100, 100)
+        form_grid.createMultiple(1, 
+            [`forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
+            `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`], [0], true);
 
+        form_grid.align(4, 4, 100, 100)
         form_grid.x = 200;
         form_grid.y = 200;
 
@@ -38,22 +40,18 @@ function create() {
         cases_2 = new Phaser.Rectangle(150, 50, 100, 100)
         cases_3 = new Phaser.Rectangle(280, 50, 100, 100)
 
-        var formes = []
-        game.add.image(0, 0, 'forme1')
-        game.add.image(100, 0, 'forme2')
-        game.add.image(200, 0, 'forme3')
-        game.add.image(300, 0, 'forme4')
-        game.add.image(400, 0, 'forme5')
-        game.add.image(500, 0, 'forme6')
+        game.add.image(0, 0, `forme${game.rnd.integerInRange(1, 6)}`)
+        game.add.image(100, 0, `forme${game.rnd.integerInRange(1, 6)}`)
+        game.add.image(200, 0, `forme${game.rnd.integerInRange(1, 6)}`)
 
         for (var i = 0; i < 6; i++) {
             formes.push(`forme${i}`)
         }     
     }
     function render() {
-        game.debug.geom(cases_1, '#03f8fc')
-        game.debug.geom(cases_2, '#03f8fc')
-        game.debug.geom(cases_3, '#03f8fc')
+        game.debug.geom(cases_1, '#0CCA4A ')
+        game.debug.geom(cases_2, '#F3FCF0')
+        game.debug.geom(cases_3, '#F3FCF0')
     }    
 
     function update() {
