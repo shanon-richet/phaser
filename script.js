1b4bc84e238316dd91d6cf7dd1b640c42d7ec0be
Shanon <shanon.richet@gmail.com>
2022-03-29 11:58:59 +0200

    add form selector

--- script.js
+++ script.js
@@ -11,17 +11,22 @@ var game = new Phaser.Game(1000, 600, Phaser.CANVAS,
     })
 
 var background
-const form_list = []
+var form_1, form_2, form_3
+var selector_1, selector_2, selector_3
+
 function preload() {
     game.stage.backgroundColor = "#427AA1"
     for (var i = 0; i < 7; i++) {
         game.load.image(`forme${i}`, `assets/forme${i}.png`)  
         const list = game.load.spritesheet('formes', `assets/forme${i}.png`, 32, 32)
-        form_list.push(list)
+        list
     }  
 }
 function create() {
-        var form_grid = game.add.group();
+        var form_grid = game.add.group()
+        var selector_group = game.add.group()
+        var form_group = game.add.group()
+        
         form_grid.createMultiple(1, 
             [`forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
             `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
@@ -36,22 +41,23 @@ function create() {
         form_grid.x = 200;
         form_grid.y = 200;
 
-        cases_1 = new Phaser.Rectangle(20, 50, 100, 100)
-        cases_2 = new Phaser.Rectangle(150, 50, 100, 100)
-        cases_3 = new Phaser.Rectangle(280, 50, 100, 100)
+        selector_1 = new Phaser.Rectangle(20, 50, 150, 150)
+        selector_2 = new Phaser.Rectangle(200, 50, 150, 150)
+        selector_3 = new Phaser.Rectangle(380, 50, 150, 150)
 
-        game.add.image(0, 0, `forme${game.rnd.integerInRange(1, 6)}`)
-        game.add.image(100, 0, `forme${game.rnd.integerInRange(1, 6)}`)
-        game.add.image(200, 0, `forme${game.rnd.integerInRange(1, 6)}`)
-
-        for (var i = 0; i < 6; i++) {
-            formes.push(`forme${i}`)
-        }     
+        form_1 = game.add.sprite(0, 0, `forme${game.rnd.integerInRange(1, 6)}`)
+        form_2 = game.add.sprite(100, 0, `forme${game.rnd.integerInRange(1, 6)}`)
+        form_3 = game.add.sprite(200, 0, `forme${game.rnd.integerInRange(1, 6)}`)
+  
+        form_1.alignIn(selector_1, Phaser.CENTER)
+        form_2.alignIn(selector_2, Phaser.CENTER)
+        form_3.alignIn(selector_3, Phaser.CENTER)
     }
     function render() {
-        game.debug.geom(cases_1, '#0CCA4A ')
-        game.debug.geom(cases_2, '#F3FCF0')
-        game.debug.geom(cases_3, '#F3FCF0')
+        game.debug.rectangle(selector_1, '#ffffff', false)
+        game.debug.rectangle(selector_2, '#ffffff', false)
+        game.debug.rectangle(selector_3, '#ffffff', false)
+        // game.debug.geom(selector_1, '#fffff')
     }    
 
     function update() {
