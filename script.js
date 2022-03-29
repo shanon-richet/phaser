b08726f849032bc2327cae9d14fa5d66106e9db9
Shanon <shanon.richet@gmail.com>
2022-03-29 15:19:00 +0200

    add background to selectors

--- script.js
+++ script.js
@@ -16,16 +16,14 @@ var selector_1, selector_2, selector_3
 
 function preload() {
     game.stage.backgroundColor = "#427AA1"
-    for (var i = 0; i < 7; i++) {
+    for (var i = 1; i < 7; i++) {
         game.load.image(`forme${i}`, `assets/forme${i}.png`)  
-        const list = game.load.spritesheet('formes', `assets/forme${i}.png`, 32, 32)
-        list
+
     }  
 }
 function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE)
         var form_grid = game.add.group()
-        var selector_group = game.add.group()
-        var form_group = game.add.group()
         
         form_grid.createMultiple(1, 
             [`forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
@@ -36,7 +34,6 @@ function create() {
             `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
             `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`,
             `forme${game.rnd.integerInRange(1, 6)}`, `forme${game.rnd.integerInRange(1, 6)}`], [0], true);
-
         form_grid.align(4, 4, 100, 100)
         form_grid.x = 200;
         form_grid.y = 200;
@@ -45,6 +42,16 @@ function create() {
         selector_2 = new Phaser.Rectangle(200, 50, 150, 150)
         selector_3 = new Phaser.Rectangle(380, 50, 150, 150)
 
+        map = game.add.bitmapData(0, 0)
+        map.rect(20, 50, 120, 120, '#fff')
+        map_2 = game.add.bitmapData(500, 500)
+        map_2.rect(200, 50, 120, 120, '#fff')
+        map_3 = game.add.bitmapData(800, 800)
+        map_3.rect(380, 50, 120, 120, '#fff')
+        map.addToWorld()
+        map_2.addToWorld()
+        map_3.addToWorld()
+
         form_1 = game.add.sprite(0, 0, `forme${game.rnd.integerInRange(1, 6)}`)
         form_2 = game.add.sprite(100, 0, `forme${game.rnd.integerInRange(1, 6)}`)
         form_3 = game.add.sprite(200, 0, `forme${game.rnd.integerInRange(1, 6)}`)
@@ -52,13 +59,15 @@ function create() {
         form_1.alignIn(selector_1, Phaser.CENTER)
         form_2.alignIn(selector_2, Phaser.CENTER)
         form_3.alignIn(selector_3, Phaser.CENTER)
+        game.physics.arcade.enable(form_1)
     }
     function render() {
         game.debug.rectangle(selector_1, '#ffffff', false)
         game.debug.rectangle(selector_2, '#ffffff', false)
         game.debug.rectangle(selector_3, '#ffffff', false)
-        // game.debug.geom(selector_1, '#fffff')
+        //game.debug.geom(selector_1, '#fffff')
     }    
 
     function update() {
+        game.world.bringToTop(form_1)
     }
